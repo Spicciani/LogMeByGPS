63726c7ce572c553a4f0a5be346131976724e60d
Spicciani <marco.spicciani@gmail.com>
2022-07-05 23:30:34 +0200

    Initial Commit

---
+++ LogMeByGPS/LogMeByGPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogMeByGPSApp.swift
+//  LogMeByGPS
+//
+//  Created by MacMini on 05/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LogMeByGPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
